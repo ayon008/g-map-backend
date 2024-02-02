673c161513843c63f350bcbbd9ddcb24b442526b
ayon008 <shariar.ayon128@gmail.com>
2024-02-02 14:35:49 +0600

    user-added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var cors = require('cors')
 require('dotenv').config()
 var app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -10,6 +11,48 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('server running')
 })
+
+const uri = "mongodb+srv://afsrun771:2f3le4QqQlcifPLL@cluster0.gdhe0uf.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        const dataBase = client.db('afsRun')
+        const userCollection = dataBase.collection('users');
+
+
+        app.post('/users', async (req, res) => {
+            const data = req.body;
+            const result = await userCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.get('/users', async (req, res) => {
+            const data = await userCollection.find().toArray()
+            res.send(data);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(port);
