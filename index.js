55e2550fe88e0be6b1a3156a94c5f4ba72b847ce
ayon008 <shariar.ayon128@gmail.com>
2024-04-26 16:08:46 +0600

    verification-added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 var jwt = require('jsonwebtoken')
 require('dotenv').config()
 var cors = require('cors')
@@ -42,6 +42,42 @@ const verify = (req, res, next) => {
     })
 }
 
+// Send verification
+// Create a transporter with Gmail credentials
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: '18eee060@gmail.com', // Replace with your Gmail address
+        pass: 'cmecrylylofkyhrd' // Replace with your Gmail password
+    }
+});
+
+const token = "7796a5384e4dfc5c2d1e3edc7f7c68e7674d29de7c5ae56762cb23f26fa564c8e91de2978da0a0052ec74072d573b46432ed2cfcaa581ea76fbf456efd858896";
+
+const generateVerificationLink = () => {
+    return `http://localhost:5173/signUp/signUpForm?token=${token}`;
+};
+const verificationLink = generateVerificationLink();
+
+// Function to send verification email
+const sendVerificationEmail = async (recipientEmail) => {
+    const mailOptions = {
+        from: '18eee060@gmail.com',
+        to: recipientEmail,
+        subject: 'Email Verification',
+        text: `Please click the following link to verify your email address: ${verificationLink}`,
+        html: `<p>Please click the following link to verify your email address: <a href="${verificationLink}">Verify Email</a></p>`
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+        return info.response;
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
+
 
 app.get('/', (req, res) => {
     res.send('server running')
@@ -54,6 +90,7 @@ async function run() {
         // user Database
         const userCollection = dataBase.collection('users');
         // Runs Database
+        // 
         const runsCollection = dataBase.collection('allRuns');
 
         // Upload User
@@ -73,19 +110,6 @@ async function run() {
             const data = await userCollection.find().toArray()
             res.send(data);
         })
-
-        // GET SPECIFIC USERS
-        // app.get('/users/:id', verify, async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { uid: { $eq: id } };
-        //     const decoded = req.decoded;
-        //     const data = await userCollection.findOne(query);
-        //     console.log(data, id);
-        //     if (data?.email === decoded.email) {
-        //         res.send(data)
-        //     }
-        // })
-
 
         // UPDATE USER INFORMATIONS
         app.put('/users/:id', async (req, res) => {
@@ -129,7 +153,23 @@ async function run() {
             res.send({ token })
         })
 
+        // userName
+        app.post('/userName', async (req, res) => {
+            const userName = req.body.userName;
+            const query = { userName: { $eq: userName } }
+            const find = await userCollection.findOne(query)
+            if (find) {
+                return res.send({ result: true })
+            }
+            res.send({ result: false })
+        })
 
+        // send verification email
+        app.post('/email', async (req, res) => {
+            const email = req.body.email;
+            sendVerificationEmail(email);
+            res.send({ result: true })
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -144,3 +184,4 @@ run().catch(console.dir);
 app.listen(port, () => {
     console.log('Afs run is running', port);
 })
+
